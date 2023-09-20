960d2f8349a6381162c15c3434f59eae4fe5ea32
DenisRudiy <denryd2311@gmail.com>
2023-09-20 21:21:27 +0300

    add btns + animated

--- src/Components/LSidebar/LSidebar.tsx
+++ src/Components/LSidebar/LSidebar.tsx
@@ -2,7 +2,28 @@ import React from "react";
 import "./LSidebar.scss";
 
 export function LSidebar() {
-  return <div className="LSidebar"></div>;
+  return (
+    <div className="LSidebar">
+      <img src="https://i.postimg.cc/nhvTqcg6/icons8-crystal-100.png" alt="" />
+      <div className="NavSection">
+        <button className="DefaultBtn">
+          <img src="https://i.postimg.cc/9fTBRzdq/akar-icons-home.png" alt="" />
+        </button>
+        <button className="DefaultBtn">
+          <img src="https://i.postimg.cc/qRTx80wD/Group.png" alt="" />
+        </button>
+        <button className="UncommonBtn">
+          <img src="https://i.postimg.cc/8zxHzL6S/sms.png" alt="" />
+        </button>
+        <button className="DefaultBtn">
+          <img src="https://i.postimg.cc/wj12zw32/la-user-friends.png" alt="" />
+        </button>
+        <button className="DefaultBtn">
+          <img src="https://i.postimg.cc/LsvTSG2J/Mask-group.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default LSidebar;
